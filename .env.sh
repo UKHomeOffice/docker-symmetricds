c71ccc3275589c0e5d29c667bddaa544486335f4
Ben Marvell <madmarv187@gmail.com>
2017-11-06 17:05:36 +0000

    -w isn't supported on a mac :(

--- .env.sh
+++ .env.sh
@@ -1,8 +1,8 @@
 #! /bin/env bash
 
 cat << END-OF-LINE
-SOURCE_CRT=`cat .docker-compose/source.crt | base64 -w0`
-SOURCE_KEY=`cat .docker-compose/source.key | base64 -w0`
-TARGET_CRT=`cat .docker-compose/target.crt | base64 -w0`
-TARGET_KEY=`cat .docker-compose/target.key | base64 -w0`
+SOURCE_CRT=`cat .docker-compose/source.crt | base64 | tr -d '\n'`
+SOURCE_KEY=`cat .docker-compose/source.key | base64 | tr -d '\n'`
+TARGET_CRT=`cat .docker-compose/target.crt | base64 | tr -d '\n'`
+TARGET_KEY=`cat .docker-compose/target.key | base64 | tr -d '\n'`
 END-OF-LINE
