a0229e1a9c85bd9e789a0d37fcb7a9ed1004e2f5
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-05-23 11:53:58 +0100

    Fix: Use HTTPS protocol for readiness probe
    There was a bug in which the readiness probe always used HTTP even when
    SymmetricDS was in HTTPS mode. This fixes that to alter the protocol
    depending on the mode.

--- readinessProbe.sh
+++ readinessProbe.sh
@@ -3,11 +3,14 @@ set -eo pipefail
 
 LISTEN_PORT="${LISTEN_PORT}"
 HTTPS="${HTTPS:-TRUE}"
+PROTOCOL="https"
 
 if [ "${HTTPS}" == "FALSE" ]; then
   LISTEN_PORT="${LISTEN_PORT:-31415}"
+  PROTOCOL="http"
 else
   LISTEN_PORT="${LISTEN_PORT:-31417}"
+  PROTOCOL="https"
 fi
 
-curl -H "Accept: application/json" http://127.0.0.1:${LISTEN_PORT}/api/engine/status | jq -e '.started == true'
+curl -H "Accept: application/json" "${PROTOCOL}://127.0.0.1:${LISTEN_PORT}/api/engine/status" | jq -e '.started == true'
